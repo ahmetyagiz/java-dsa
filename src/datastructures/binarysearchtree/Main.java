63fb0afb122f4efb462beadaad13343f8b850c09
ahmetyagiz <ahmetanteryagiz@gmail.com>
2025-03-06 21:22:15 +0300

    Create BinarySearchTree

---
+++ src/datastructures/binarysearchtree/Main.java
@@ -0,0 +1,30 @@
+package datastructures.binarysearchtree;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinarySearchTree myBST = new BinarySearchTree();
+
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        myBST.insert(27);
+
+        System.out.println("Root: " + myBST.root.value);
+        System.out.println(myBST.root.left.right.value);
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Root: 47
+            27
+        */
+
+    }
+
+}
