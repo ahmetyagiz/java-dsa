dde86822690842b9d7535cc8404fd7bf23f57d51
ahmetyagiz <ahmetanteryagiz@gmail.com>
2025-03-07 22:20:40 +0300

    Create Graph

---
+++ src/datastructures/graph/Main.java
@@ -0,0 +1,31 @@
+package datastructures.graph;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Graph myGraph = new Graph();
+
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+
+        myGraph.addEdge("A", "B");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("B", "C");
+
+        myGraph.removeVertex("A", "B");
+        System.out.println("\nGraph:");
+        myGraph.printGraph();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Graph:
+            {A=[C], B=[C], B=[A, B]}
+
+        */
+
+    }
+
+}
